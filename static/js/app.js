be1cefb431486232c839acf72aadf9db0427b866
Neel Chunara <126720049+neelchunara@users.noreply.github.com>
2023-08-07 20:30:12 -0400

    Belly Button Biodiversity Dashboard

---
+++ static/js/app.js
@@ -0,0 +1,87 @@
+// It wii read json data and uses it to populate the dropdown. 
+
+d3.json("https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json").then(function(incomingData) {
+    
+    //Populate the dropdown
+    d3.select("#selDataset")
+        .selectAll("option")
+        .data(incomingData.names)
+        .enter()
+        .append("option")
+        .text(d=>d)
+        .attr("value",d=>d);
+
+    optionChanged(d3.select("#selDataset").property("value"));
+});
+
+// It will create bar chart, feed the top 10 OTUs and the labels.
+
+function CreateHBar(x,y,text) {
+    var data = [{
+        type: 'bar',
+        x: x,
+        y: y,
+        text: text,
+        orientation: 'h'
+    }];
+
+    var layout = {
+        title: "Top 10 OTUs"
+      };
+
+    Plotly.newPlot('bar', data, layout);
+}
+
+// It will create bubble chart
+
+function CreateBubble(x,y,text) {
+    var data = [{
+        x: x,
+        y: y,
+        text: text,
+        mode: 'markers',
+        marker: {
+          size: y,
+          color: x.map(value=>value)
+        }
+    }];
+    var layout = {
+        title: "OTU Values",
+        xaxis: {
+            title: {
+              text: 'OTU ID',
+            }
+        }
+    };
+    Plotly.newPlot('bubble', data, layout);
+}
+
+
+// This function can feed the metadata.
+
+function Meta(data) {
+    var div = d3.select("#sample-metadata");
+    div.html("")
+    var list = div.append("ul");
+    Object.entries(data).forEach(([key, value]) => {
+        list.append("li").text(key + ": " + value);
+     });
+}
+
+// This 'master function' loads in the json data and executes each function so all charts are populated.
+function optionChanged(value) {
+    d3.json("https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json").then(function(incomingData) {
+        var metadata = incomingData.metadata.filter(data => data.id ==value);
+        console.log(metadata);
+
+        var sample = incomingData.samples.filter(data => data.id ==value);
+        console.log(sample);
+
+        CreateHBar(sample[0].sample_values.slice(0,10).reverse(),sample[0].otu_ids.slice(0,10).reverse().map(a=>"OTU "+ a),sample[0].otu_labels.slice(0,10).reverse());
+        CreateBubble(sample[0].otu_ids,sample[0].sample_values,sample[0].otu_labels);
+        Meta(metadata[0]);
+        CreateGauge(metadata[0].wfreq);
+    });
+
+
+}
